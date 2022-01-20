f5e7f0dfb89950c8f5f6331cd27927bcbc9044e3
Marharitavol <marharitavol@gmail.com>
2022-01-20 19:35:54 +0200

    Initial Commit

---
+++ FindNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindNews
+//
+//  Created by Rita on 20.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
